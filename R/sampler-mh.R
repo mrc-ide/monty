7a7c27dee0758cec7e76c4dd7bd05038564d79d3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-02-15 16:58:06 +0000

    Add basic MH sampler

---
+++ R/sampler-mh.R
@@ -0,0 +1,63 @@
+##' Create a simple Metropolis-Hastings sampler (really just a plain
+##' Metropolis sampler since we assume symmetric proposals and don't
+##' yet support the correction for asymmetric proposal).
+##'
+##' @title Metropolis-Hastings Sampler
+##'
+##' @param proposal A proposal function; must take a vector of
+##'   parameters and produce a new vector of proposed parameters.
+##'
+##' @param vcv A variance covariance matrix to generate a `proposal`
+##'   function from.  If you want multivariate Gaussian proposal, this
+##'   is likely simpler than supplying your own `proposal`, and
+##'   generally more efficient too.
+##'
+##' @return A `mcstate_sampler` object, which can be used with
+##'   [mcstate_sample]
+##'
+##' @export
+mcstate_sampler_metropolis_hastings <- function(proposal = NULL, vcv = NULL) {
+  if (is.null(proposal) && is.null(vcv)) {
+    cli::cli_abort("One of 'proposal' or 'vcv' must be given")
+  }
+  if (!is.null(proposal) && !is.null(vcv)) {
+    cli::cli_abort("Only one of 'proposal' or 'vcv' may be given")
+  }
+  if (!is.null(vcv)) { # proposal is null
+    check_vcv(vcv, call = rlang::current_env())
+    proposal <- make_rmvnorm(vcv)
+  }
+
+  initialise <- function(state, model) {
+    if (!is.null(vcv)) {
+      n_pars <- length(state$pars)
+      n_vcv <- nrow(vcv)
+      if (n_pars != n_vcv) {
+        cli::cli_abort(c("Invalid length parameters or vcv",
+                         i = "Given {n_pars} parameter{?s}",
+                         i = "Given {n_vcv} x {n_vcv} vcv"))
+      }
+    }
+  }
+
+  step <- function(state, model) {
+    density_accept <- state$density + log(runif(1))
+    pars_next <- proposal(state$pars)
+
+    ## We might want to do a few things here to pass in density_accept,
+    ## and to indicate that we want to use the model (if provided) and
+    ## not the prior here.  We might also want to report all of the
+    ## three probabilities here, as at the moment we don't allow that
+    ## decomposition.
+    density_next <- model$density(pars_next)
+    if (density_next > density_accept) {
+      state$pars <- pars_next
+      state$density <- density_next
+    }
+    state
+  }
+
+  mcstate_sampler("Metropolis-Hastings",
+                  initialise,
+                  step)
+}
---
+++ R/sampler.R
@@ -0,0 +1,78 @@
+##' Sample from a model.  Uses a Monte Carlo method (or possibly
+##' something else in future) to generate samples from your
+##' distribution.  This is going to change a lot in future, as we add
+##' support for distributing over workers, and for things like
+##' parallel reproducible streams of random numbers.  For now it just
+##' runs a single chain as a proof of concept.
+##'
+##' @title Sample from a model
+##'
+##' @param model The model to sample from; this should be a
+##'   `mcstate_model` for now, but we might change this in future to
+##'   test to see if things match an interface rather than a
+##'   particular class attribute.
+##'
+##' @param sampler A sampler to use.  These will be described later,
+##'   but we hope to make these reasonably easy to implement so that
+##'   we can try out different sampling ideas.  For now, the only
+##'   sampler implemented is [mcstate_sampler_metropolis_hastings()].
+##'
+##' @param n_steps The number of steps to run the sampler for.
+##'
+##' @param initial Optionally, initial parameter values for the
+##'   sampling.  If not given, we sample from the model (or its prior).
+##'
+##' @return A list of parameters and densities.
+##'
+##' @export
+mcstate_sample <- function(model, sampler, n_steps, initial = NULL) {
+  if (is.null(initial)) {
+    ## Really this would just be from the prior; we can't directly
+    ## sample from the posterior!
+    pars <- model$sample()
+  } else {
+    pars <- initial
+    if (length(pars) != length(model$parameters)) {
+      cli::cli_abort(
+        c("Unexpected initial parameter length"),
+        arg = "initial")
+    }
+  }
+
+  if (!inherits(model, "mcstate_model")) {
+    cli::cli_abort("Expected 'model' to be a 'mcstate_model'",
+                   arg = "model")
+  }
+  if (!inherits(sampler, "mcstate_sampler")) {
+    cli::cli_abort("Expected 'sampler' to be a 'mcstate_sampler'",
+                   arg = "sampler")
+  }
+
+  density <- model$density(pars)
+  state <- list(pars = pars, density = density)
+  sampler$initialise(state, model)
+
+  history_pars <- matrix(NA_real_, n_steps + 1, length(pars))
+  history_pars[1, ] <- pars
+  history_density <- rep(NA_real_, n_steps + 1)
+  history_density[[1]] <- density
+
+  for (i in seq_len(n_steps)) {
+    state <- sampler$step(state, model)
+    history_pars[i + 1, ] <- state$pars
+    history_density[[i + 1]] <- state$density
+  }
+
+  ## Pop the parameter names on last
+  colnames(history_pars) <- model$parameters
+
+  list(pars = history_pars,
+       density = history_density)
+}
+
+
+mcstate_sampler <- function(name, initialise, step) {
+  ret <- list(name = name, initialise = initialise, step = step)
+  class(ret) <- "mcstate_sampler"
+  ret
+}
