b6308b6370a3bef1a79620b46dd19165e4d72a56
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-02-16 09:41:18 +0000

    Simpler rmvnorm

--- R/distributions.R
+++ R/distributions.R
@@ -1,9 +1,6 @@
 ## Not in base R
 rmvnorm <- function(x, vcv) {
-  n <- ncol(vcv)
-  r <- chol(vcv, pivot = TRUE)
-  r <- r[, order(attr(r, "pivot", exact = TRUE))]
-  x + drop(rnorm(n) %*% r)
+  make_rmvnorm(vcv)(x)
 }
 
 
