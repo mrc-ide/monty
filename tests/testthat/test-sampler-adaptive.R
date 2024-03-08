fb976b8fb895da35d5ca6793f7c487b783247444
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-03-08 14:19:45 +0000

    Store parameters in multidimensional array

--- tests/testthat/test-sampler-adaptive.R
+++ tests/testthat/test-sampler-adaptive.R
@@ -5,12 +5,13 @@ test_that("Empirical VCV calculated correctly with forget_rate = 0", {
                                       forget_rate = 0,
                                       log_scaling_update = FALSE)
   res <- mcstate_sample(m, sampler, 1000)
-  expect_equal(names(res), c("pars", "density", "initial", "details", "chain"))
+  expect_equal(names(res), c("pars", "density", "initial", "details"))
 
   ## forget_rate = 0 so full chain should be included in VCV
   expect_equal(res$details[[1]]$weight, 1000)
   expect_equal(res$details[[1]]$included, seq_len(1000))
-  expect_equal(res$details[[1]]$vcv, cov(res$pars), ignore_attr = TRUE)
+  pars <- t(array_drop(res$pars, 2))
+  expect_equal(res$details[[1]]$vcv, cov(pars), ignore_attr = TRUE)
 })
 
 
@@ -20,12 +21,13 @@ test_that("Empirical VCV calculated correctly with forget_rate = 0.1", {
   sampler <- mcstate_sampler_adaptive(initial_vcv = diag(c(0.01, 0.01)),
                                       forget_rate = 0.1)
   res <- mcstate_sample(m, sampler, 1000)
-  expect_equal(names(res), c("pars", "density", "initial", "details", "chain"))
+  expect_equal(names(res), c("pars", "density", "initial", "details"))
 
   ## forget_rate = 0.1 so VCV should exclude first 100 parameter sets
   expect_equal(res$details[[1]]$weight, 900)
   expect_equal(res$details[[1]]$included, seq(101, 1000, by = 1))
-  expect_equal(res$details[[1]]$vcv, cov(res$pars[101:1000, ]),
+  pars <- t(array_drop(res$pars, 2))
+  expect_equal(res$details[[1]]$vcv, cov(pars[101:1000, ]),
                ignore_attr = TRUE)
 })
 
@@ -37,13 +39,14 @@ test_that("Empirical VCV correct using both forget_rate and forget_end", {
                                       forget_rate = 0.5,
                                       forget_end = 200)
   res <- mcstate_sample(m, sampler, 1000)
-  expect_equal(names(res), c("pars", "density", "initial", "details", "chain"))
+  expect_equal(names(res), c("pars", "density", "initial", "details"))
 
   ## forget_rate = 0.5 and forget_end = 200 so VCV should exclude first
   ## 100 parameter sets
   expect_equal(res$details[[1]]$weight, 900)
   expect_equal(res$details[[1]]$included, seq(101, 1000, by = 1))
-  expect_equal(res$details[[1]]$vcv, cov(res$pars[101:1000,]),
+  pars <- t(array_drop(res$pars, 2))
+  expect_equal(res$details[[1]]$vcv, cov(pars[101:1000,]),
                ignore_attr = TRUE)
 })
 
@@ -56,13 +59,14 @@ test_that("Empirical VCV correct using forget_rate, forget_end and adapt_end", {
                                       forget_end = 100,
                                       adapt_end = 300)
   res <- mcstate_sample(m, sampler, 1000)
-  expect_equal(names(res), c("pars", "density", "initial", "details", "chain"))
+  expect_equal(names(res), c("pars", "density", "initial", "details"))
 
   ## forget_rate = 0.25, forget_end = 500 and adapt_end = 300 so VCV should
   ## only include parameter sets 26 to 300
   expect_equal(res$details[[1]]$weight, 275)
   expect_equal(res$details[[1]]$included, seq(26, 300, by = 1))
-  expect_equal(res$details[[1]]$vcv, cov(res$pars[26:300,]),
+  pars <- t(array_drop(res$pars, 2))
+  expect_equal(res$details[[1]]$vcv, cov(pars[26:300,]),
                ignore_attr = TRUE)
 })
 
