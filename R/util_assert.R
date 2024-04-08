e59db73fd448f96bf76cd4a0f2daeb3add73bca7
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-04-08 14:30:53 +0100

    Harmonise utilities with #27

--- R/util_assert.R
+++ R/util_assert.R
@@ -22,7 +22,7 @@ assert_scalar <- function(x, name = deparse(substitute(x)), arg = name,
 assert_logical <- function(x, name = deparse(substitute(x)),
                           arg = name, call = NULL) {
   if (!is.logical(x)) {
-    cli::cli_abort("Exected '{name}' to be logical", arg = arg, call = call)
+    cli::cli_abort("Expected '{name}' to be logical", arg = arg, call = call)
   }
   invisible(x)
 }
@@ -31,7 +31,7 @@ assert_logical <- function(x, name = deparse(substitute(x)),
 assert_character <- function(x, name = deparse(substitute(x)),
                              arg = name, call = NULL) {
   if (!is.character(x)) {
-    cli::cli_abort("'{name}' must be character", call = call, arg = arg)
+    cli::cli_abort("Expected '{name}' to be character", call = call, arg = arg)
   }
   invisible(x)
 }
@@ -40,7 +40,7 @@ assert_character <- function(x, name = deparse(substitute(x)),
 assert_nonmissing <- function(x, name = deparse(substitute(x)),
                           arg = name, call = NULL) {
   if (anyNA(x)) {
-    cli::cli_abort("Exected '{name}' to be non-NA", arg = arg, call = call)
+    cli::cli_abort("Expected '{name}' to be non-NA", arg = arg, call = call)
   }
   invisible(x)
 }
--- tests/testthat/test-util-assert.R
+++ tests/testthat/test-util-assert.R
@@ -7,8 +7,15 @@ test_that("assert_scalar", {
 
 test_that("assert_character", {
   expect_silent(assert_character("a"))
-  expect_error(assert_character(1), "must be character")
-  expect_error(assert_character(TRUE), "must be character")
+  expect_error(assert_character(1), "to be character")
+  expect_error(assert_character(TRUE), "to be character")
+})
+
+
+test_that("assert_logical", {
+  expect_silent(assert_logical(TRUE))
+  expect_error(assert_logical(1), "Expected '1' to be logical")
+  expect_error(assert_logical("a"), "Expected '\"a\"' to be logical")
 })
 
 
