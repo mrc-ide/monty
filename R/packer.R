c7d0bc5479ac6d6a7028ccec109b0b9cc6248571
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-06-10 08:34:50 +0100

    New, more descriptive name

--- R/packer.R
+++ R/packer.R
@@ -1,9 +1,86 @@
-##' Build a parameter transformer, which can be used in models to
-##' translate between an unstructured vector of numbers (the vector
-##' being updated by an MCMC for example) to a structured list of
-##' named values, which is easier to program against.
+##' Build a parameter packer, which can be used in models to translate
+##' between an unstructured vector of numbers (the vector being
+##' updated by an MCMC for example) to a structured list of named
+##' values, which is easier to program against.  We refer to the
+##' process of taking a named list of scalars, vectors and arrays and
+##' converting into a single vector "packing" and the inverse
+##' "unpacking".
 ##'
-##' @title Build a parameter transformer
+##' There are several places where it is most convenient to work in an
+##' unstructured vector:
+##'
+##' * An MCMC is typically discussed as a the updating of some
+##'   vector `x` to another `x'`
+##' * An optimisation algorithm will try and find a set of values for
+##'   a vector `x` that minimises (or maximises) some function `f(x)`
+##' * An ode solver works with a vector `x(t)` (`x` at time `t`) and
+##'   considers `x(t + h)` by computing the vector of derivatives
+##'   `dx(t)/dt`
+##'
+##' In all these cases, the algorithm that needs the vector of numbers
+##' knows nothing about what they represent.  Commonly, these will be
+##' a packed vector of parameters.  So our vector `x` might actually
+##' represent the parameters `a`, `b` and `c` in a vector as `[a, b,
+##' c]` - this is a very common pattern, and you have probably
+##' implemented this yourself.
+##'
+##' In more complex settings, we might want our vector `x` to collect
+##' more structured quantities.  Suppose that you are fitting a model
+##' with an age-structured or sex-structured parameter.  Rather than
+##' having a series of scalars packed into your vector `x` you might
+##' have a series of values destined to be treated as a vector:
+##'
+##' ```
+##' | 1  2  3  4  5  6  7  |
+##' | a  b  c  d1 d2 d3 d4 |
+##' ```
+##'
+##' So here we might have a vector of length 7, where the first three
+##' elements will represent be the scalar values `a`, `b` and `c` but
+##' ther next four will be a vector `d`.
+##'
+##' Unpacked, this might be written as:
+##'
+##' ```
+##' list(a = 1, b = 2, c = 3, d = 4:7)
+##' ```
+##'
+##' The machinery here is designed to make these transformations
+##' simple and standardised within mcstate2, and should be flexible
+##' enough for many situations.  We will also use these from within
+##' `dust2` and `odin2` for transformations in and out of vectors of
+##' ODE state.
+##'
+##' # When to use `process`
+##'
+##' The `process` function is a get-out-of-jail function designed to
+##' let you do arbitrary transformations when unpacking a vector.  In
+##' general, this should not be the first choice to use because it is
+##' less easy to reason about by other tooling (for example, as we
+##' develop automatic diferentiation support for use with the HMC
+##' algorithm, a `process` function will be problematic because we
+##' will need to make sure we can differentiate this process).
+##' However, there are cases where it will be only way to achieve some
+##' results.
+##'
+##' Imagine that you are packing a 2x2 covariance matrix into your
+##' vector in order to use within an MCMC or optimisation algorithm.
+##' Ultimately, our unpacked vector will need to hold four elements
+##' (`b11`, `b12`, `b21`, `b22`), but there are only three distinct
+##' values as the two off-diagonal elements will be the same (i.e.,
+##' `b12 == b21``).  So we might write this passing in `b_raw = 3` to
+##' `array`, so that our unpacked list holds `b_raw = c(b11, b12,
+##' b22)`.  We would then write `process` as something like:
+##'
+##' ```
+##' process <- function(x) {
+##'   list(b = matrix(x$b_raw[c(1, 2, 2, 3)], 2, 2))
+##' }
+##' ```
+##'
+##' which creates the symmetric 2x2 matrix `b` from `b_raw`.
+##'
+##' @title Build a parameter packer
 ##'
 ##' @param scalar Names of scalar parameters.  This is similar for
 ##'   listing elements in `array` with values of 1, though elements in
@@ -28,27 +105,26 @@
 ##'   entries, which will be concatenated onto the original list.  If
 ##'   you use this you should take care not to return any values with
 ##'   the same names as entries listed in `scalar`, `array` or
-##'   `fixed`, as this is an error (this is so that `untransform()` is
+##'   `fixed`, as this is an error (this is so that `pack()` is
 ##'   not broken).  We will likely play around with this process in
 ##'   future in order to get automatic differentiation to work.
 ##'
-##' @return An object of class `mcstate_transformer`, which has three
+##' @return An object of class `mcstate_packer`, which has three
 ##'   elements:
 ##'
 ##' * `parameters`: a character vector of computed parameter names;
 ##'   these are the names that your statistical model will use.
-##' * `transform`: a function that can transform from your statistical
-##'   parameters into a structured list of parameters.
-##' * `untransform`: a function that can untransform from your
-##'   structured list of parameters back into a numeric vector
-##'   suitable for the statistical model.  This ignores values created by
-##'   a `preprocess` function.
+##' * `unpack`: a function that can unpack an unstructured vector
+##'   (say, from your statistical model parameters) into a structured
+##'   list (say, for your generative model)
+##' * `pack`: a function that can pack your structured list of
+##'   parameters back into a numeric vector suitable for the
+##'   statistical model.  This ignores values created by a
+##'   `preprocess` function.
 ##'
 ##' @export
-mcstate_transformer <- function(scalar = NULL,
-                                array = NULL,
-                                fixed = NULL,
-                                process = NULL) {
+mcstate_packer <- function(scalar = NULL, array = NULL, fixed = NULL,
+                           process = NULL) {
   call <- environment()
 
   parameters <- character(0)
@@ -76,7 +152,7 @@ mcstate_transformer <- function(scalar = NULL,
   if (!is.null(array)) {
     assert_named(array, unique = TRUE, call = call)
     for (nm in names(array)) {
-      tmp <- transform_array(nm, array[[nm]], call)
+      tmp <- prepare_pack_array(nm, array[[nm]], call)
       parameters <- c(parameters, tmp$names)
       shape[[nm]] <- tmp$shape
       idx[[nm]] <- seq_len(tmp$n) + len
@@ -99,7 +175,7 @@ mcstate_transformer <- function(scalar = NULL,
 
   if (length(idx) == 0) {
     cli::cli_abort(
-      c("Trying to generate an empty transformer",
+      c("Trying to generate an empty packer",
         i = paste("You have not provided any entries in 'scalar' or 'array',",
                   "which implies generating from a zero-length parameter",
                   "vector")))
@@ -113,7 +189,7 @@ mcstate_transformer <- function(scalar = NULL,
     }
   }
 
-  transform <- function(x) {
+  unpack <- function(x) {
     if (!is.null(names(x))) {
       if (!identical(names(x), parameters)) {
         ## Here, we could do better I think with this message; we
@@ -147,31 +223,37 @@ mcstate_transformer <- function(scalar = NULL,
             x = "{?Entry/Entries} already present: {squote(err)}"))
       }
       ## TODO: check names?
+      ##
+      ## TODO: this fails the multi-region use - but I think that we
+      ## might want a different interface there anyway as we'll
+      ## struggle to hold all the options here.
       res <- c(res, extra)
     }
     res
   }
 
-  untransform <- function(p) {
+  pack <- function(p) {
     res <- numeric(length(parameters))
     if (!all(lengths(p[names(idx)]) == lengths(idx))) {
       ## Not quite enough, because we should check the dimensions too.
       ## That ends up being quite hard with integer checks possibly
       ## because we really want to use identical() - this will do for now.
-      cli::cli_abort("Invalid structure to untransform")
+      cli::cli_abort("Invalid structure to pack()")
     }
     unlist(lapply(names(idx), function(el) p[el]), TRUE, FALSE)
   }
 
   ret <- list(parameters = parameters,
-              transform = transform,
-              untransform = untransform)
-  class(ret) <- "mcstate_transformer"
+              unpack = unpack,
+              pack = pack)
+  class(ret) <- "mcstate_packer"
   ret
 }
 
 
-transform_array <- function(name, shape, call = NULL) {
+## Helper function to create array bookkeeping for the
+## unpacking/packing process.
+prepare_pack_array <- function(name, shape, call = NULL) {
   if (length(shape) == 0) {
     cli::cli_abort(
       paste("Elements of 'array' must have at least one element, but",
---
+++ tests/testthat/test-packer.R
@@ -0,0 +1,146 @@
+test_that("can't create empty packer", {
+  expect_error(mcstate_packer(),
+               "Trying to generate an empty packer")
+})
+
+
+test_that("trivial packer", {
+  xp <- mcstate_packer("a")
+  expect_equal(xp$parameters, "a")
+  expect_equal(xp$unpack(1), list(a = 1))
+  expect_equal(xp$unpack(c(a = 1)), list(a = 1))
+  expect_error(xp$unpack(c(b = 1)),
+               "Incorrect names in input")
+  expect_error(xp$unpack(1:2),
+               "Incorrect length input; expected 1 but given 2")
+  expect_equal(xp$pack(list(a = 1)), 1)
+  expect_error(xp$pack(list(a = 1:2)),
+               "Invalid structure to unpack")
+})
+
+
+test_that("multiple scalar unpacking", {
+  xp <- mcstate_packer(c("a", "b", "c"))
+  expect_equal(xp$parameters, c("a", "b", "c"))
+  expect_equal(xp$unpack(1:3), list(a = 1, b = 2, c = 3))
+  expect_equal(xp$pack(list(a = 1, b = 2, c = 3)), 1:3)
+})
+
+
+test_that("can bind data into an unpacked list", {
+  xp <- mcstate_packer(c("a", "b"), fixed = list(x = 1:5, y = 10))
+  expect_equal(xp$parameters, c("a", "b"))
+  expect_equal(xp$unpack(1:2), list(a = 1, b = 2, x = 1:5, y = 10))
+  expect_equal(xp$pack(list(a = 1, b = 2, x = 1:5, y = 10)), 1:2)
+  expect_equal(xp$pack(list(a = 1, b = 2)), 1:2)
+})
+
+
+test_that("can unpack arrays", {
+  xp <- mcstate_packer("a", list(b = 3))
+  expect_equal(xp$parameters, c("a", "b[1]", "b[2]", "b[3]"))
+  expect_equal(xp$unpack(1:4), list(a = 1, b = 2:4))
+})
+
+
+test_that("can use integer vectors for array inputs", {
+  xp <- mcstate_packer("a", c(b = 3, c = 4))
+  expect_equal(xp$parameters,
+               c("a", sprintf("b[%d]", 1:3), sprintf("c[%d]", 1:4)))
+  expect_equal(xp$unpack(1:8), list(a = 1, b = 2:4, c = 5:8))
+})
+
+
+test_that("can create packer with only arrays", {
+  xp <- mcstate_packer(array = list(a = 2, b = 3))
+  expect_equal(xp$unpack(1:5), list(a = 1:2, b = 3:5))
+})
+
+
+test_that("can create packers with higher-level dimensionsality", {
+  xp <- mcstate_packer(
+    array = list(a = 1, b = 2, c = 2:3, d = 2:4))
+  expect_equal(
+    xp$parameters,
+    c("a[1]", "b[1]", "b[2]",
+      sprintf("c[%d,%d]", 1:2, rep(1:3, each = 2)),
+      sprintf("d[%d,%d,%d]", 1:2, rep(1:3, each = 2), rep(1:4, each = 6))))
+  expect_equal(
+    xp$unpack(1:33),
+    list(a = 1, b = 2:3, c = matrix(4:9, 2, 3), d = array(10:33, 2:4)))
+  expect_equal(
+    xp$pack(xp$unpack(1:33)),
+    1:33)
+})
+
+
+test_that("names for scalar, array and fixed must be reasonable", {
+  expect_error(
+    mcstate_packer(c("a", "b", "a")),
+    "Elements of 'scalar' must be unique")
+  expect_error(
+    mcstate_packer("a", list(1)),
+    "'array' must be named")
+  expect_error(
+    mcstate_packer("a", list(b = 1, c = 2, b = 3)),
+    "'array' must have unique names")
+  expect_error(
+    mcstate_packer("a", fixed = list(x = 1, y = 2, x = 1)),
+    "'fixed' must have unique names")
+  expect_error(
+    mcstate_packer(c("a", "b", "c"), list(b = 2, d = 3)),
+    "Names must be distinct between 'scalar', 'array' and 'fixed'")
+  expect_error(
+    mcstate_packer(c("a", "b", "c"), list(d = 2, e = 3), list(d = 5)),
+    "Names must be distinct between 'scalar', 'array' and 'fixed'")
+})
+
+
+test_that("validate array inputs", {
+  expect_error(
+    mcstate_packer(array = list(a = integer(), b = 2)),
+    "Elements of 'array' must have at least one element, but 'a' has none")
+  expect_error(
+    mcstate_packer(array = list(a = 1, b = 2.5)),
+    "Elements of 'array' must be integer-like vectors, but 'b' is not")
+  expect_error(
+    mcstate_packer(array = list(a = 1, b = c(3, 0, 2))),
+    "All dimensions in 'array' must be at least 1, but 'b' violates this")
+})
+
+
+test_that("require that scalar is a character vector", {
+  expect_error(
+    mcstate_packer(1:2),
+    "Expected a character vector for 'scalar'")
+})
+
+
+test_that("can post-process parameters", {
+  p <- function(x) {
+    list(d = x$a + x$b + x$c)
+  }
+  xp <- mcstate_packer(c("a", "b", "c"), process = p)
+  expect_equal(xp$parameters, c("a", "b", "c"))
+  expect_equal(xp$unpack(1:3), list(a = 1, b = 2, c = 3, d = 6))
+  expect_equal(xp$pack(xp$unpack(1:3)), 1:3)
+  expect_equal(xp$pack(list(a = 1, b = 2, c = 3)), 1:3)
+})
+
+
+test_that("reject non-function process arguments", {
+  expect_error(
+    mcstate_packer(c("a", "b", "c"), process = TRUE),
+    "Expected a function for 'process'")
+})
+
+
+test_that("require that process is well-behaved", {
+  p <- function(x) {
+    c(x, list(d = x$a + x$b + x$c))
+  }
+  xp <- mcstate_packer(c("a", "b", "c"), process = p)
+  expect_error(xp$unpack(1:3),
+               "'process()' is trying to overwrite entries in parameters",
+               fixed = TRUE)
+})
