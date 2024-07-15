115c5fe9c37f33b1b38e1f0e9aa4facf345ce9d0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-07-15 11:52:44 +0100

    Generate sample functions from dsl

--- tests/testthat/test-dsl.R
+++ tests/testthat/test-dsl.R
@@ -14,3 +14,29 @@ test_that("can generate model with simple assignment", {
   expect_s3_class(m, "mcstate_model")
   expect_equal(m$density(0), dnorm(0, 5, 1, log = TRUE))
 })
+
+
+test_that("can sample from a simple model", {
+  m <- mcstate_dsl("a ~ Normal(0, 1)")
+  expect_s3_class(m, "mcstate_model")
+  expect_true(m$properties$has_direct_sample)
+
+  r <- mcstate_rng$new(seed = 42)
+  cmp <- r$normal(1, 0, 1)
+
+  r <- mcstate_rng$new(seed = 42)
+  expect_equal(m$direct_sample(r), cmp)
+})
+
+
+test_that("can sample from a model with assignments", {
+  m <- mcstate_dsl({
+    mu <- 5
+    a ~ Normal(mu, 1)
+  })
+  r <- mcstate_rng$new(seed = 42)
+  cmp <- r$normal(1, 5, 1)
+
+  r <- mcstate_rng$new(seed = 42)
+  expect_equal(m$direct_sample(r), cmp)
+})
