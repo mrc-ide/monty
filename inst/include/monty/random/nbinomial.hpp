2c184dacfdebda17cecfccf4c22c8d350349cf00
edknock <edwardknock@gmail.com>
2024-10-18 10:10:04 +0100

    update negative binomial functions

--- inst/include/monty/random/nbinomial.hpp
+++ inst/include/monty/random/nbinomial.hpp
@@ -12,11 +12,11 @@ namespace random {
 namespace {
 
 template <typename real_type>
-void nbinomial_validate(real_type size, real_type prob) {
+void negative_binomial_validate(real_type size, real_type prob) {
    if(!R_FINITE(size) || !R_FINITE(prob) || size <= 0 || prob <= 0 || prob > 1) {
     char buffer[256];
     snprintf(buffer, 256,
-             "Invalid call to nbinomial with size = %g, prob = %g",
+             "Invalid call to negative_binomial with size = %g, prob = %g",
              size, prob);
     monty::utils::fatal_error(buffer);
   }
@@ -25,16 +25,27 @@ void nbinomial_validate(real_type size, real_type prob) {
 }
 
 template <typename real_type, typename rng_state_type>
-real_type nbinomial(rng_state_type& rng_state, real_type size, real_type prob) {
+real_type negative_binomial_prob(rng_state_type& rng_state, real_type size, real_type prob) {
 #ifdef __CUDA_ARCH__
-  static_assert("nbinomial() not implemented for GPU targets");
+  static_assert("negative_binomial_prob() not implemented for GPU targets");
 #endif
-    nbinomial_validate(size, prob);
+    negative_binomial_validate(size, prob);
 
     if (rng_state.deterministic) {
       return (1 - prob) * size / prob;
     }
     return (prob == 1) ? 0 : poisson(rng_state, gamma_scale(rng_state, size, (1 - prob) / prob));
+}
+
+template <typename real_type, typename rng_state_type>
+real_type negative_binomial_mu(rng_state_type& rng_state, real_type size, real_type mu) {
+#ifdef __CUDA_ARCH__
+  static_assert("negative_binomial_mu() not implemented for GPU targets");
+#endif
+  const auto prob = size / (size + mu);
+  negative_binomial_validate(size, prob);
+  
+  return negative_binomial_prob(rng_state, size, prob);
 }
 
 }
