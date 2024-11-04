3b2135c1324614350026708648e4eea9ba3b285c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-11-04 14:34:58 +0000

    Simplify docs

--- R/runner.R
+++ R/runner.R
@@ -7,11 +7,8 @@
 ##' @param progress Optional logical, indicating if we should print a
 ##'   progress bar while running.  If `NULL`, we use the value of the
 ##'   option `monty.progress` if set, otherwise we show the progress
-##'   bar (as it is typically wanted).  The progress bar itself
-##'   responds to cli's options; in particular
-##'   `cli.progress_show_after` and `cli.progress_clear` will affect
-##'   your experience.  Alternatively, you can provide a string
-##'   indicating the progress bar type.  Options are `fancy`
+##'   bar (as it is typically wanted).  Alternatively, you can provide
+##'   a string indicating the progress bar type.  Options are `fancy`
 ##'   (equivalent to `TRUE`), `none` (equivalent to `FALSE`) and
 ##'   `simple` (a very simple text-mode progress indicator designed
 ##'   play nicely with logging; it does not use special codes to clear
