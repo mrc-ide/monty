f9ae68569bb0d4b9aaf78dd8519f2101a2574335
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-11-08 09:40:57 +0000

    Reset accept swap each time

--- tests/testthat/test-parallel-tempering.R
+++ tests/testthat/test-parallel-tempering.R
@@ -6,9 +6,7 @@ test_that("can run a PT sampler", {
   posterior <- likelihood + prior
   s <- monty_parallel_tempering(n_rungs = 10, vcv = matrix(0.1))
   res <- monty_sample(posterior, s, 100, n_chains = 4)
-
-  hist(c(res$pars), freq = FALSE)
-  x <- seq(min(res$pars), max(res$pars), length.out = 1001)
-  y <- exp(posterior$density(rbind(x)))
-  lines(x, y / sum(y) / diff(x)[[1]], col = "red")
+  expect_type(res$details, "list")
+  expect_equal(names(res$details[[1]]), "accept_swap")
+  expect_length(res$details[[1]]$accept_swap, 10)
 })
