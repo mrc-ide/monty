080092ed1f9b5a4616a1f64a330f0cdfe600899c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-12-11 21:11:29 +0000

    Remove most of the legacy interface

--- src/test_rng.cpp
+++ src/test_rng.cpp
@@ -4,8 +4,10 @@
 
 #include <cpp11.hpp>
 
+
 #include <monty/random/generator.hpp>
-#include <monty/r/random.hpp>
+#include <monty/random/prng.hpp>
+
 template <typename T>
 std::string to_string(const T& t) {
   std::ostringstream ss;
@@ -13,9 +15,11 @@ std::string to_string(const T& t) {
   return ss.str();
 }
 
+// We could do this in R space instead
 template <typename T>
-std::vector<std::string> test_xoshiro_run1(cpp11::environment ptr) {
-  auto rng = monty::random::r::rng_pointer_get<T>(ptr, 1);
+std::vector<std::string> test_xoshiro_run1(cpp11::sexp ptr) {
+  using default_rng64 = monty::random::prng<monty::random::xoshiro256plus>;
+  auto rng = cpp11::as_cpp<cpp11::external_pointer<default_rng64>>(ptr).get();
   auto& state = rng->state(0);
 
   constexpr int n = 10;
@@ -35,34 +39,11 @@ std::vector<std::string> test_xoshiro_run1(cpp11::environment ptr) {
 }
 
 [[cpp11::register]]
-std::vector<std::string> test_xoshiro_run(cpp11::environment obj) {
-  const auto algorithm = cpp11::as_cpp<std::string>(obj["algorithm"]);
+std::vector<std::string> test_xoshiro_run(cpp11::sexp obj, std::string algorithm) {
   std::vector<std::string> ret;
-  if (algorithm == "xoshiro256starstar") {
-    ret = test_xoshiro_run1<monty::random::xoshiro256starstar>(obj);
-  } else if (algorithm == "xoshiro256plusplus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro256plusplus>(obj);
-  } else if (algorithm == "xoshiro256plus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro256plus>(obj);
-  } else if (algorithm == "xoshiro128starstar") {
-    ret = test_xoshiro_run1<monty::random::xoshiro128starstar>(obj);
-  } else if (algorithm == "xoshiro128plusplus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro128plusplus>(obj);
-  } else if (algorithm == "xoshiro128plus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro128plus>(obj);
-  } else if (algorithm == "xoroshiro128starstar") {
-    ret = test_xoshiro_run1<monty::random::xoroshiro128starstar>(obj);
-  } else if (algorithm == "xoroshiro128plusplus") {
-    ret = test_xoshiro_run1<monty::random::xoroshiro128plusplus>(obj);
-  } else if (algorithm == "xoroshiro128plus") {
-    ret = test_xoshiro_run1<monty::random::xoroshiro128plus>(obj);
-  } else if (algorithm == "xoshiro512starstar") {
-    ret = test_xoshiro_run1<monty::random::xoshiro512starstar>(obj);
-  } else if (algorithm == "xoshiro512plusplus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro512plusplus>(obj);
-  } else if (algorithm == "xoshiro512plus") {
-    ret = test_xoshiro_run1<monty::random::xoshiro512plus>(obj);
+  if (algorithm != "xoshiro256plus") {
+    cpp11::stop("Algorithm '%s' not supported", algorithm.c_str());
   }
-
+  ret = test_xoshiro_run1<monty::random::xoshiro256plus>(obj);
   return ret;
 }
--- tests/testthat/test-xoshiro.R
+++ tests/testthat/test-xoshiro.R
@@ -1,14 +1,13 @@
 test_that("xoshiro output agrees with reference data", {
   path <- "xoshiro-ref"
   status <- list()
-  for (name in dir(path)) {
-    obj <- monty_rng_pointer$new(seed = 42, algorithm = name)
-    res <- test_xoshiro_run(obj)
-    obj$sync()
-    len <- if (grepl("^xoshiro128", name)) 4 else 8
-    s <- matrix(obj$state(), len)[rev(seq_len(len)), ]
-    s_str <- apply(s, 2, paste, collapse = "")
-    cmp <- readLines(sprintf("%s/%s", path, name))
-    expect_equal(c(res, s_str), cmp)
-  }
+  name <- "xoshiro256plus"
+
+  obj <- monty_rng_create(seed = 42)
+  res <- test_xoshiro_run(obj, name)
+  len <- 8
+  s <- matrix(monty_rng_state(obj), len)[rev(seq_len(len)), ]
+  s_str <- apply(s, 2, paste, collapse = "")
+  cmp <- readLines(sprintf("%s/%s", path, name))
+  expect_equal(c(res, s_str), cmp)
 })
