74fea44ff440a40a8e8fbf5fcca1b1314abca9f1
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-07-18 13:38:28 +0100

    More relaxed binomial check

--- inst/include/monty/random/binomial.hpp
+++ inst/include/monty/random/binomial.hpp
@@ -198,7 +198,10 @@ real_type btrs(rng_state_type& rng_state, real_type n, real_type p) {
 template <typename real_type>
 __host__ __device__
 void binomial_validate(real_type n, real_type p) {
-  if (n < 0 || p < 0 || p > 1 || std::isnan(n) || std::isnan(p)) {
+  const bool err = n < 0 || p < 0 || p > 1 ||
+    (std::isnan(p) && n > 0) ||
+    (std::isnan(n) && p > 0);
+  if (err) {
     char buffer[256];
     snprintf(buffer, 256,
              "Invalid call to binomial with n = %.0f, p = %g, q = %g",
