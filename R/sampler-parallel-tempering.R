3e16bd019ab0aab2dd5e6d786a2eec32a7773011
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-09-17 15:46:39 +0100

    Add tuning of parallel tempering

--- R/sampler-parallel-tempering.R
+++ R/sampler-parallel-tempering.R
@@ -36,6 +36,32 @@
 ##' limit, a normal sampler may only explore a single peak in a model
 ##' with many such peaks and will never mix properly.
 ##'
+##' # Tuning the beta values
+##'
+##' The argument `beta` controls the spacing of temperature among
+##' chains.  Ideally, this is set so that adjacent chains all have the
+##' same probability of swapping, which generally means that the
+##' `beta` values themselves will *not* be equidistant.  Creating the
+##' appropriate vector of betas (or "annealing schedule") requires
+##' running a pilot run, then computing new `beta` values, then
+##' running another (new) chain with the new beta values, repeating
+##' this iteration a few times until the beta values stabilise.
+##' Rumour has it, this should only take a few iterations.
+##'
+##' The updated values of `beta` are stored in the `details` of your
+##' samples after running.  So if you have a set of samples called
+##' `s`, then `s$details$beta` will contain new beta values that you
+##' can use on the next run.
+##'
+##' It is important not to concatenate chains that are computed with
+##' different `beta` values as they will not generally represent
+##' samples from the target distribution (as an extreme case, consider
+##' one set of beta values where we never accept swaps onto the target
+##' distribution and another where we always do; we obviously cannot
+##' concatenate these).  Therefore, every time that you change beta
+##' values you should start a new chain.  You can do this with
+##' [monty_sample_continue] so long as you pass `append = FALSE`.
+##'
 ##' @title Parallel Tempering Sampler
 ##'
 ##' @param n_rungs The number of **extra** chains to run, must be at
@@ -96,6 +122,26 @@ monty_sampler_parallel_tempering <- function(sampler, n_rungs = NULL,
                 sampler_parallel_tempering_restore,
                 sampler_parallel_tempering_details,
                 properties = properties)
+}
+
+
+
+sampler_parallel_tempering_update_beta <- function(beta,
+                                                   accept_swap,
+                                                   attempt_swap) {
+  ## Compute rejection rate (between 0 and 1) for swaps, averaged over
+  ## all chains.  There is an unavoidable fencepost error here, but we
+  ## anchor this at zero which is what the interpolation needs.
+  r <- c(0, 1 - rowSums(accept_swap) / rowSums(attempt_swap))
+
+  ## Compute normalised lambda (normalised cumulative rejection rate)
+  lambda <- cumsum(r / sum(r))
+
+  ## The new betas are an interpolated set of beta values that would
+  ## have given an equally spaced set of lambdas -- which is equal
+  ## rejection rates for each pair:
+  suppressWarnings(
+    approx(lambda, beta, seq(0, 1, length.out = length(beta))))$y
 }
 
 
@@ -297,9 +343,12 @@ sampler_parallel_tempering_restore <- function(chain_id, state_chain,
 
 
 sampler_parallel_tempering_details <- function(state, control) {
+  beta_new <- sampler_parallel_tempering_update_beta(
+    control$beta, state$accept_swap, state$attempt_swap)
   list(accept_swap = state$accept_swap,
        attempt_swap = state$attempt_swap,
-       sampler = control$sampler$state$details(state$sub_state_sampler))
+       sampler = control$sampler$state$details(state$sub_state_sampler),
+       beta = beta_new)
 }
 
 
