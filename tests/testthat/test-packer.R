5b8041a54921097eab6fdde7cd83a0ac34e4743e
edknock <edwardknock@gmail.com>
2025-10-15 14:03:29 +0100

    Tst unpacking matrix when unpacker uses fixed

--- tests/testthat/test-packer.R
+++ tests/testthat/test-packer.R
@@ -213,6 +213,15 @@ test_that("can't used process with array unpacking", {
 })
 
 
+test_that("can't used fixed with array unpacking", {
+  p <- monty_packer(c("x", "y"), fixed = list(z = 3))
+  expect_equal(p$unpack(1:2), list(x = 1, y = 2, z = 3))
+  expect_error(
+    p$unpack(matrix(1:6, 2)),
+    "Can't unpack a matrix where the unpacker uses 'fixed'")
+})
+
+
 test_that("Properly unpack scalars stored as zero-length arrays", {
   p <- monty_packer(array = list(a = integer(0), b = 1L))
   expect_equal(p$unpack(1:2), list(a = 1, b = 2))
