7fc7f8c9dddcb8510e1bb11e82beebad97ca80cf
edknock <edwardknock@gmail.com>
2025-12-15 16:13:09 +0000

    remove bit accidentally pushed

--- inst/include/monty/random/math.hpp
+++ inst/include/monty/random/math.hpp
@@ -505,27 +505,5 @@ T sign(T x) {
   return (T(0) < x) - (x < T(0));
 }
 
-
-inline double fmodr(double x, double y) {
-  const auto ret = std::fmod(x, y);
-  if (ret * y < 0) {
-    ret += y;
-  }
-  return ret;
-}
-
-inline float fmodr(float x, float y) {
-  const auto ret = std::fmodf(x, y);
-  if (ret * y < 0) {
-    ret += y;
-  }
-  return ret;
-}
-
-template <typename real_type>
-real_type fintdiv(real_type x, real_type y) {
-  return monty::math::floor(x / y);
-}
-
 }
 }
