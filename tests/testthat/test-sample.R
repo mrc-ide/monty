4fac144aa6b71a0eefe331e1b1b40e0c96e481a5
edknock <edwardknock@gmail.com>
2026-04-28 14:21:27 +0100

    Fix test

--- tests/testthat/test-sample.R
+++ tests/testthat/test-sample.R
@@ -19,7 +19,8 @@ test_that("sampler return value contains history", {
   res <- monty_sample(model, sampler, 100, 1)
   expect_setequal(
     names(res),
-    c("pars", "density", "initial", "details", "state", "observations"))
+    c("pars", "pars_full", "density", "initial",
+      "details", "state", "observations"))
   expect_equal(dim(res$pars), c(1, 100, 1))
   expect_equal(dimnames(res$pars), list("gamma", NULL, NULL))
   expect_equal(dim(res$density), c(100, 1))
