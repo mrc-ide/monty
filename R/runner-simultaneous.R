23ec8cb1f79e923775f5f7d20117fcfd2dc12b72
edknock <edwardknock@gmail.com>
2026-04-28 14:48:02 +0100

    Setup thinning in runner simultaneous

--- R/runner-simultaneous.R
+++ R/runner-simultaneous.R
@@ -139,34 +139,46 @@ monty_run_chains_simultaneous2 <- function(chain_state, sampler_state,
   initial <- chain_state$pars
   n_pars <- length(model$parameters)
   n_chains <- length(chain_state$density)
-  n_steps_record <- steps$total
+  
+  burnin <- steps$burnin
+  thinning_factor <- steps$thinning_factor
+  n_steps <- steps$total
+  n_steps_record <- ceiling((steps$total - burnin) / thinning_factor)
   save_full_chains <- steps$save_full_chains
   
   pars <- array(NA_real_, c(n_pars, n_steps_record, n_chains))
+  density <- matrix(NA_real_, n_steps_record, n_chains)
   if (save_full_chains) {
-    pars_full <- array(NA_real_, c(n_pars, n_steps_record, n_chains))
+    pars_full <- array(NA_real_, c(n_pars, n_steps, n_chains))
   } else {
     pars_full <- NULL
   }
-  density <- matrix(NA_real_, n_steps_record, n_chains)
-
+  
+  
   chain_id <- seq_len(n_chains)
 
-  for (i in seq_len(steps$total)) {
+  j <- 1L
+  for (i in seq_len(n_steps)) {
     chain_state <- sampler$step(chain_state, sampler_state, sampler$control,
                                 model, rng)
-    pars[, i, ] <- chain_state$pars
+    if (i > burnin && i %% thinning_factor == 0) {
+      pars[, j, ] <- chain_state$pars
+      density[j, ] <- chain_state$density
+      ## TODO: also allow observations here if enabled
+      j <- j + 1L
+    }
     if (save_full_chains) {
       pars_full[, i, ] <- chain_state$pars
     }
-    density[i, ] <- chain_state$density
-    ## TODO: also allow observations here if enabled
     progress(chain_id, i)
   }
 
   ## Pop the parameter names on last
   rownames(pars) <- model$parameters
-
+  if (save_full_chains) {
+    rownames(pars_full) <- model$parameters
+  }
+  
   sampler_state <- sampler$state$dump(sampler_state)
   details <- sampler$state$details(sampler_state)
 
