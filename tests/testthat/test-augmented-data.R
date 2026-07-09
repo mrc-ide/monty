474ad6a9707518b4b94232b75c30ef4a3be8f7d7
edknock <edwardknock@gmail.com>
2026-07-09 16:47:19 +0100

    Test flattening chains with augmented data

--- tests/testthat/test-augmented-data.R
+++ tests/testthat/test-augmented-data.R
@@ -14,6 +14,7 @@ test_that("can run sampler with augmented data", {
   expect_equal(dim(res$data), c(100, 10, 1))
 })
 
+
 test_that("can continue sampling with augmented_data", {
   m <- ex_augmented()
   model <- m$prior + m$likelihood
@@ -29,6 +30,7 @@ test_that("can continue sampling with augmented_data", {
   expect_equal(res2b, res1)
 })
 
+
 test_that("Can't use parallel tempering with augmented_data", {
   m <- ex_augmented()
   m$likelihood$properties$allow_multiple_parameters <- TRUE
@@ -39,4 +41,25 @@ test_that("Can't use parallel tempering with augmented_data", {
   expect_error(
     monty_sample(model, pt_sampler, 10),
     "'model' has augmented data but Parallel Tempering")
+})
+
+
+test_that("can flatten chains with augmented data", {
+  m <- ex_augmented()
+  model <- m$prior + m$likelihood
+  sampler <- monty_sampler_random_walk(vcv = diag(c(0.1, 0.5, 0.5)))
+  
+  set.seed(1)
+  res <- monty_sample(model, sampler, 10, n_chains = 2)
+  expect_equal(dim(res$data), c(100, 10, 2))
+  
+  res1 <- monty_flatten_chains(res)
+  expect_equal(dim(res1$data), c(100, 20))
+  
+  set.seed(1)
+  res2 <- monty_sample(model, sampler, 10, n_chains = 2, flatten_chains = TRUE)
+  expect_identical(res1, res2)
+  
+  res3 <- monty_unflatten_chains(res1)
+  expect_identical(res, res3)
 })
