a8759a4e97124f8bbc7c003ae7d31183e4ac290f
edknock <edwardknock@gmail.com>
2026-09-18 13:52:29 +0100

    Remove browser

--- R/sampler-random-walk.R
+++ R/sampler-random-walk.R
@@ -84,7 +84,7 @@ sampler_random_walk_initialise <- function(state_chain, control, model, rng) {
 
 sampler_random_walk_step <- function(state_chain, state_sampler, control,
                                      model, rng) {
-  browser()
+
   if (control$rerun) {
     rerun <- state_sampler$rerun(rng)
     if (any(rerun)) {
